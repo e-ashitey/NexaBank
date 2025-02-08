8e4107b3de3d7f709bd76de39fbec8e74aae9649
Emmanuel Ashitey <eaashitey39@gmail.com>
2025-02-08 19:25:14 +0000

    create permission struct

--- internal/models/admin.go
+++ internal/models/admin.go
@@ -3,5 +3,11 @@ package models
 type Admin struct {
 	Id          string
 	Role        string
-	Permissions string
+	Permissions []Permission
+}
+
+type Permission struct {
+	ID          string
+	Name        string // e.g., "view_reports"
+	Description string // e.g., "Allows viewing system-wide reports"
 }
