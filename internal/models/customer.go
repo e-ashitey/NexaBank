9378c35ab715775e4278662c7cea72f1f124e371
Emmanuel Ashitey <eaashitey39@gmail.com>
2025-02-10 15:54:56 +0000

    add validator function

--- internal/models/customer.go
+++ internal/models/customer.go
@@ -1,5 +1,7 @@
 package models
 
+import "errors"
+
 type Customer struct {
 	ID       string
 	Name     string
@@ -8,4 +10,14 @@ type Customer struct {
 	Phone    string
 	Address  string
 	Status   string
+}
+
+func Validate(c *Customer) error {
+	if c.Name == "" || len(c.Name) < 5 {
+		return errors.New("name can't be empty and must be at least 5 characters")
+	}
+	if c.Email == "" || c.Password == "" || c.Phone == "" || c.Address == "" {
+		return errors.New("all fields are required")
+	}
+	return nil
 }
